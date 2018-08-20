347c6f087d824b27f2159654adf12338ef7e0a5b
Brian Egan <bfegan@gmail.com>
2018-08-20 13:58:09 -0400

    Create new_mac_setup.sh

---
+++ project_info/new_mac_setup.sh
@@ -0,0 +1,19 @@
+## Run the below to set up on a fresh Mac user profile
+
+#Unsure if I really need this, but might...
+brew install openssl 
+
+# Add wget
+brew install wget
+
+# Install gphoto2 itself
+brew install gphoto2
+
+# Install Node.js
+brew install node
+
+
+# Download and run gphoto2 updater
+wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && sudo bash gphoto2-updater.sh
+
+# Wallah!
